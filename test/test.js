e92f27186e2717c4ceacf34b27bb3e33a9ea7b1a
hack2root <me@experimentalcommunity.org>
2018-08-02 05:38:55 +0300

    added mocha tests for VS Code editor

--- test/test.js
+++ test/test.js
@@ -1,10 +1,18 @@
-//let lazy=(eval)=>((data)=>new Proxy(data,{set(obj,key,val){obj[key]=val;eval(obj);}}))({});
+/*
+
+let lazy = (eval) => ((data) => new Proxy(data, {
+  set(obj, key, val) {
+      obj[key] = val;
+      eval(obj);
+  }
+}))({});
+
+*/
 
 var expect = require('chai').expect;
-
 var lazy = require('../lazyeval');
 
-describe('lazy', function () {
+describe('let func = lazy((func) => c = func.a + func.b)', function () {
   it('should add two numbers', function () {
 
     // 1. ARRANGE
@@ -12,38 +20,58 @@ describe('lazy', function () {
     let b = 2;
 
     // 2. ACT
-
-`    let func = lazy((func) => c = func.a + func.b);
-
+    let func = lazy((func) => c = func.a + func.b);
     func.a = a;
     func.b = b;
-    let c;
+    var c;
 
     // 3. ASSERT
     expect(c).to.be.equal(3);
   });
 });
 
-
-describe('lazy', function () {
+describe('let func = lazy(() => c = a + func.b)', function () {
   it('should add two numbers', function () {
 
     // 1. ARRANGE
-    let x = 5;
-    let y = 1;
-    let sum1 = 6;
+    let a = 1;
+    let b = 2;
 
     // 2. ACT
-    let func = lazy((func) => sum2 = func.x + func.y);
-    func.x = x;
-    func.y = y;
-
-    let sum2;
+    let func = lazy(() => c = a + func.b);
+    func.b = b;
+    var c;
 
     // 3. ASSERT
-    expect(sum2).to.be.equal(sum1);
+    expect(c).to.be.equal(3);
   });
 });
+
+describe('let func = lazy(() => c = a + func.c)', function () {
+  it('should add two numbers', function () {
+
+    // 1. ARRANGE
+    let a = 1;
+    let b = 2;
+
+    // 2. ACT
+    let func = lazy(() => c = a + func.c);
+    Object.defineProperty(func,'c', {
+      get: function() {
+        return c;
+      },
+      set: function(newVal) {
+        c = newVal;
+      }
+    })
+    func.c = 2;
+    var c;
+
+    // 3. ASSERT
+    expect(c).to.be.equal(3);
+  });
+});
+
 //let lazy=(e)=>((o)=>new Proxy(o,{set(o,k,v){o[k]=v;e(o);}}))({});
 /*
 let a = 1;
