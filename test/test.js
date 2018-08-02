fb525d9eb64a85891444e930828c7127dfe9bcc2
hack2root <me@experimentalcommunity.org>
2018-08-02 05:44:47 +0300

    removed comments

--- test/test.js
+++ test/test.js
@@ -89,25 +89,3 @@ describe('let func = lazy(() => c = a + func.c)', function () {
     expect(c).to.be.equal(3);
   });
 });
-
-//let lazy=(e)=>((o)=>new Proxy(o,{set(o,k,v){o[k]=v;e(o);}}))({});
-/*
-let a = 1;
-let b = 2;
-let c;
-let f = _(() => c = a + f.c);
-f.c = -1;
-console.log(c);
-
-Object.defineProperty(f,'c',{
-    get: function() {
-        return c;
-    },
-    set: function(newVal) {
-        c = newVal;
-    }
-})
-
-f.c = 2;
-console.log(c);
-*/
